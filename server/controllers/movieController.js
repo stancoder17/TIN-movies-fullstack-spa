70dbd6abb97477309ef859f439082333e64fa171
stani <stanislawdyrda7@gmail.com>
2025-12-13 18:02:42 +0100

    Add movieController.js with CRUD

---
+++ server/controllers/movieController.js
@@ -0,0 +1,55 @@
+const Movie = require('../models/Movie.js');
+
+const getAllMovies = async (req, res) => {
+    try {
+        const movies = await Movie.getAll();
+
+        res.status(200).json(movies);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Server error while fetching movies' });    }
+}
+
+const getMovieById = async (req, res) => {
+    try {
+        const id = req.params.id;
+        const movie = await Movie.getById(id);
+
+        if (!movie) {
+            res.status(404).json({ message: `Movie with id ${id} not found` });
+            return;
+        }
+
+        res.status(200).json(movie);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Server error while fetching movie' });
+    }
+}
+
+const createMovie = async (req, res) => {
+    try {
+        const movie = req.body;
+
+        const result = await Movie.create(movie);
+        res.status(201).json(result);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Server error while creating movie' });
+    }
+}
+
+const updateMovie = async (req, res) => {
+    try {
+        const id = req.params.id;
+        const movie = req.body;
+
+        const result = await Movie.update(id, movie);
+        res.status(200).json(result);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Server error while updating movie' });
+    }
+}
+
+export { getAllMovies, getMovieById, createMovie, updateMovie };
