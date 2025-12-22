2d0e1595a15eb1eb4de2d3158c7127c86f05c421
stani <stanislawdyrda7@gmail.com>
2025-12-22 18:06:00 +0100

    Add header and footer components

--- client/src/components/footer/TopXMovies.jsx
+++ client/src/components/footer/TopXMovies.jsx
@@ -11,7 +11,7 @@ function TopXMovies() {
             .then(response => response.json())
             .then(data => setMovies(data))
             .catch(error => console.error(`Error fetching top ${count} movies:`, error))
-    })
+    }, [])
 
     return (
         <div className="link-group">
